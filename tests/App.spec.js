dc284601a88635f51886475951e695fa18e9838a
王昱 <wangyu2021@foxmail.com>
2025-07-02 15:40:51 +0300

    11.9

--- tests/App.spec.js
+++ tests/App.spec.js
@@ -1,23 +1,23 @@
-const { test, describe, expect } = require("@playwright/test");
+const { test, describe, expect } = require('@playwright/test')
 
-describe("Pokedex", () => {
-  test("front page can be opened", async ({ page }) => {
-    await page.goto("");
-    await expect(page.getByText("ivysaur")).toBeVisible();
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(
       page.getByText(
-        "Pokémon and Pokémon character names are trademarks of Nintendo."
+        'Pokémon and Pokémon character names are trademarks of Nintendo.'
       )
-    ).toBeVisible();
-  });
+    ).toBeVisible()
+  })
 
-  test("user can navigate from the main page to a specific pokemon page", async ({
+  test('user can navigate from the main page to a specific pokemon page', async ({
     page,
   }) => {
-    await page.goto("");
+    await page.goto('')
 
-    await page.getByText("ivysaur").click();
+    await page.getByText('ivysaur').click()
 
-    await expect(page.getByText("chlorophyll")).toBeVisible();
-  });
-});
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
+})
