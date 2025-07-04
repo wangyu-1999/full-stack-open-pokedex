55da5654ca2653e2c70c0a41bb97127bb207d653
王昱 <wangyu2021@foxmail.com>
2025-07-04 10:36:58 +0300

    11.12

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5001
 
-app.use(express.static('dist'))
+app.get("/health", (req, res) => {
+  res.send("ok");
+});
 
-app.get('/health', (req, res) => {
-  res.send('ok')
-})
+app.use(express.static("dist"));
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
