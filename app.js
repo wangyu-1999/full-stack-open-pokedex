f517010118f385ee5d86a94bf1713cdac0f2cdca
王昱 <wangyu2021@foxmail.com>
2025-07-04 10:49:31 +0300

    11.12

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 5001
 
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
-  if (true) throw "error...  ";
-  res.send("ok");
+  if (true) throw 'error...  '
+  res.send('ok')
 })
 
 app.use(express.static('dist'))
